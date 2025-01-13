a51150c8bec65590b7ef3b4f883444f09ff8cf79
macs <rakeshreddyd56@gmail.com>
2025-01-12 21:15:39 -0800

    postgressql

--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class ScanHistory(Base):
     membership = relationship('Membership', back_populates='scan_history')
 
 # Manually configure database URL
-DATABASE_URL = "postgres://u5tcjrchmc5fa3:p24192f96879635b1330d21133a01a2d3777838f27821f755df3849609c9889e1@cfls9h51f4i86c.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/da9iac6pkpkurm"
+DATABASE_URL = "postgresql://u5tcjrchmc5fa3:p24192f96879635b1330d21133a01a2d3777838f27821f755df3849609c9889e1@cfls9h51f4i86c.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/da9iac6pkpkurm"
 
 # Initialize the database
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True enables SQL logging for debugging
