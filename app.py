0a0d70dded01c7bf1c9c0f43a6240d3eeb549627
PicklePlexHyd <pickleplexhyd@gmail.com>
2025-02-04 10:33:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def admin_scan():
 
     return render_template("admin_scan.html", member=member)
 
-@app.route('/pass/<string:membership_id>')
+@app.route('/pass/<string:membership_id>', methods=['GET', 'POST'])
 def show_pass(membership_id):
     print(f"Accessing pass for Membership ID: {membership_id}")
     try:
